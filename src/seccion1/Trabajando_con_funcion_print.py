04cdff95e71c9147bd76372ea7239c1e5f00c777
Simon0811 <simonvasquez550@gmail.com>
2025-07-31 13:36:05 -0500

    Seccion 1:Laboratorio 1

---
+++ src/seccion1/Trabajando_con_funcion_print.py
@@ -0,0 +1,13 @@
+print("Hola mundo");
+#se hizo un print y dentro de comillas dobles se coloco el mensaje a mostrar.
+
+print("Simon");
+#Se hizo un print y dentro de las comillas puse mi nombre.
+
+print(Simon);
+#El error que aparece es que "Simon" no esta definido.
+
+print "Simon"
+#El error que aparece es que las declaraciones deben ir separadas por nuevas lineas o punto y coma
+
+
